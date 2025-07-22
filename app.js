df40921dc7f3e6d41965f3b4393f95f8acc0107b
IshmoelM <mufti.ishmoel@gmail.com>
2025-07-22 21:39:17 +0500

    resetbutton

--- app.js
+++ app.js
@@ -1,63 +1,64 @@
-const cell = document.querySelectorAll('.cell');
-
-let Player = "X";
-
-let GameOver = false;
-
-const WinningCombination = [
-[0 , 1 , 2],
-[3 , 4 , 5],
-[6 , 7 , 8],
-[0 , 3 , 6],
-[1 , 4 , 7],
-[2 , 5 , 8],
-[0 , 4 , 8],
-[2 , 4 , 6]
+const cells = document.querySelectorAll('.cell');
+let currentPlayer = "X";
+let isGameOver = false;
+const winningCombinations = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
 ];
-
-cell.forEach((cell , index ) => {
-    cell .addEventListener('click', () => handleClick(cell ,index))
-
+cells.forEach((cell, index) => {
+  cell.addEventListener('click', () => handleClick(cell, index));
 });
-
-function handleClick(cell , index)
-{
-    if(GameOver || cell.textContent !== '')
- return;
-cell.textContent = Player;
-
-if(checkWin()) {
-    alert('${Player} wins!')
-    isGameOver = true ;
+function handleClick(cell, index) {
+  if (isGameOver || cell.textContent !== '') return;
+  cell.textContent = currentPlayer;
+  if (checkWin()) {
+    alert(`${currentPlayer} wins!`);
+    isGameOver = true;
     return;
-
-
+  }
+  if (checkDraw()) {
+    alert("It's a draw!");
+    isGameOver = true;
+    return;
+  }
+  currentPlayer = currentPlayer === "X" ? "O" : "X";
 }
-
-    if (checkDraw()){
-        alert("its a draw")
-        isGameOver = true
-        return;
-
-    }
-
-    Player = Player === "X" ? "O" : "X"
-
-}
-
 function checkWin() {
-    return WinningCombination.some(combination => {
-        const [a , b , c] = combination
-        return(
-            cell[a].textContent === Player && 
-            cell[b].textContent === Player &&
-            cell[c].textContent === Player );
- } )
-    
-
+  return winningCombinations.some(combination => {
+    const [a, b, c] = combination;
+    return (
+      cells[a].textContent === currentPlayer &&
+      cells[b].textContent === currentPlayer &&
+      cells[c].textContent === currentPlayer
+    );
+  });
 }
-
 function checkDraw() {
-    return [...cells].every( cells => 
-    cell.textContent!== '');
+  return [...cells].every(cell => cell.textContent !== '');
 }
+
+document.getElementById('resetbutton').addEventListener('click' , resetgame)
+
+function resetgame ()
+{
+  cells.forEach(cell=>{
+    cell.textContent= '';
+  });
+
+  currentPlayer = "X";
+  isGameOver = false;
+}
+
+
+
+
+
+
+
+
